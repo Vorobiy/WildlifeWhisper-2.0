653104e6f77489b715b53098d2674b8d2f08af2b
Vorobiy <eugenevorob@gmail.com>
2024-08-18 15:28:40 -0400

    fixed fitting for devices, fixed color

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 function Header() {    
     return(
-        <header className="header-container">
-            <h1 className="header-left">Wildlife Whisper</h1>
-            <nav className="header-right">
-                <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Pinpoint Poacher</a></li>
-                    <li><a href="#">Home</a></li>
-                </ul>
-            </nav>
-        </header>
+        <div className="header-container">
+            <header>
+                <nav>
+                    <h1 className="logo">Wildlife Whisper</h1>
+                    <ul className="nav-links">
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Pinpoint Poacher</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
     );
 }
 
--- my-react-app/src/Tiger.jsx
+++ my-react-app/src/Tiger.jsx
@@ -2,15 +2,15 @@ import videofile from '../src/assets/tigervideo.mp4'
 
 function Video() {
   return (
-    <>
-    <div>
-      <video className="tiger-container" autoPlay loop muted>
-        <source src={videofile} type="video/mp4"></source>
-      </video>
-    </div>
-    <div className="tiger-content">
-      <h1>Welcome to Wildlife Whisper.</h1>
-      <h2>An anonymous, online poaching awareness system and reporting tool.</h2>
+    <> 
+    <div className="main">
+        <video autoPlay loop muted>
+          <source src={videofile} type="video/mp4"></source>
+        </video>
+      <div className="tiger-content">
+        <h1>Welcome to Wildlife Whisper.</h1>
+        <h2>An anonymous, online poaching awareness system and reporting tool.</h2>
+      </div>
     </div>
     </>
   );
